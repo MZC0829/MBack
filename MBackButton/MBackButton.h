cf2c5cf89dd08cbef8a5933ead9be5e86b289ce4
maizhichao <maizhichao5848@dingtalk.com>
2018-02-07 20:41:25 +0800

    carthage
    add carthage

---
+++ MBackButton/MBackButton.h
@@ -0,0 +1,19 @@
+//
+//  MBackButton.h
+//  MBackButton
+//
+//  Created by 麦志超 on 2018/2/7.
+//  Copyright © 2018年 MZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBackButton.
+FOUNDATION_EXPORT double MBackButtonVersionNumber;
+
+//! Project version string for MBackButton.
+FOUNDATION_EXPORT const unsigned char MBackButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBackButton/PublicHeader.h>
+
+
---
+++ MBackFramework/MBackFramework.h
@@ -0,0 +1,19 @@
+//
+//  MBackFramework.h
+//  MBackFramework
+//
+//  Created by 麦志超 on 2018/2/7.
+//  Copyright © 2018年 MZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBackFramework.
+FOUNDATION_EXPORT double MBackFrameworkVersionNumber;
+
+//! Project version string for MBackFramework.
+FOUNDATION_EXPORT const unsigned char MBackFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBackFramework/PublicHeader.h>
+
+
